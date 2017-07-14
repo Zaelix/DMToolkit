63cc67f8f55f355ba257f5d668dd136c3aeddb8c
Michael Johnson <zaelix@gmail.com>
2017-07-13 17:39:28 -0700

    Initial initiative tracking added
    -Simple Characters can now be created and added to the character list,
    with a name and initiative score.
    -Simple Characters can be edited, or added to the initiative tracker.
    -Initiative tracker can be sorted by initiative score.

---
+++ DMToolkit/DMTCore.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace DMToolkit
+{
+    partial class DMTool
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.combatButton = new System.Windows.Forms.Button();
+            this.combatPanel = new System.Windows.Forms.Panel();
+            this.initTrackerSortButton = new System.Windows.Forms.Button();
+            this.delCharButton = new System.Windows.Forms.Button();
+            this.editCharButton = new System.Windows.Forms.Button();
+            this.charListLabel = new System.Windows.Forms.Label();
+            this.charInitLabel = new System.Windows.Forms.Label();
+            this.charInitBox = new System.Windows.Forms.NumericUpDown();
+            this.createCharButton = new System.Windows.Forms.Button();
+            this.charNameBox = new System.Windows.Forms.TextBox();
+            this.charListBox = new System.Windows.Forms.ListBox();
+            this.addToTrackerButton = new System.Windows.Forms.Button();
+            this.initTracker = new System.Windows.Forms.ListView();
+            this.initiativeTrackerLabel = new System.Windows.Forms.Label();
+            this.combatPanel.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.charInitBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // combatButton
+            // 
+            this.combatButton.Location = new System.Drawing.Point(13, 13);
+            this.combatButton.Name = "combatButton";
+            this.combatButton.Size = new System.Drawing.Size(75, 23);
+            this.combatButton.TabIndex = 0;
+            this.combatButton.Text = "Combat";
+            this.combatButton.UseVisualStyleBackColor = true;
+            this.combatButton.Click += new System.EventHandler(this.combatButton_Click);
+            // 
+            // combatPanel
+            // 
+            this.combatPanel.Controls.Add(this.initTrackerSortButton);
+            this.combatPanel.Controls.Add(this.delCharButton);
+            this.combatPanel.Controls.Add(this.editCharButton);
+            this.combatPanel.Controls.Add(this.charListLabel);
+            this.combatPanel.Controls.Add(this.charInitLabel);
+            this.combatPanel.Controls.Add(this.charInitBox);
+            this.combatPanel.Controls.Add(this.createCharButton);
+            this.combatPanel.Controls.Add(this.charNameBox);
+            this.combatPanel.Controls.Add(this.charListBox);
+            this.combatPanel.Controls.Add(this.addToTrackerButton);
+            this.combatPanel.Controls.Add(this.initTracker);
+            this.combatPanel.Controls.Add(this.initiativeTrackerLabel);
+            this.combatPanel.Location = new System.Drawing.Point(13, 43);
+            this.combatPanel.Name = "combatPanel";
+            this.combatPanel.Size = new System.Drawing.Size(1092, 571);
+            this.combatPanel.TabIndex = 1;
+            // 
+            // initTrackerSortButton
+            // 
+            this.initTrackerSortButton.Location = new System.Drawing.Point(286, 46);
+            this.initTrackerSortButton.Name = "initTrackerSortButton";
+            this.initTrackerSortButton.Size = new System.Drawing.Size(84, 23);
+            this.initTrackerSortButton.TabIndex = 12;
+            this.initTrackerSortButton.Text = "Sort Tracker";
+            this.initTrackerSortButton.UseVisualStyleBackColor = true;
+            this.initTrackerSortButton.Click += new System.EventHandler(this.initTrackerSortButton_Click);
+            // 
+            // delCharButton
+            // 
+            this.delCharButton.Enabled = false;
+            this.delCharButton.Location = new System.Drawing.Point(747, 21);
+            this.delCharButton.Name = "delCharButton";
+            this.delCharButton.Size = new System.Drawing.Size(95, 23);
+            this.delCharButton.TabIndex = 11;
+            this.delCharButton.Text = "Delete Character";
+            this.delCharButton.UseVisualStyleBackColor = true;
+            // 
+            // editCharButton
+            // 
+            this.editCharButton.Enabled = false;
+            this.editCharButton.Location = new System.Drawing.Point(654, 21);
+            this.editCharButton.Name = "editCharButton";
+            this.editCharButton.Size = new System.Drawing.Size(84, 23);
+            this.editCharButton.TabIndex = 10;
+            this.editCharButton.Text = "Edit Character";
+            this.editCharButton.UseVisualStyleBackColor = true;
+            this.editCharButton.Click += new System.EventHandler(this.editCharButton_Click);
+            // 
+            // charListLabel
+            // 
+            this.charListLabel.AutoSize = true;
+            this.charListLabel.Location = new System.Drawing.Point(382, 4);
+            this.charListLabel.Name = "charListLabel";
+            this.charListLabel.Size = new System.Drawing.Size(72, 13);
+            this.charListLabel.TabIndex = 9;
+            this.charListLabel.Text = "Character List";
+            // 
+            // charInitLabel
+            // 
+            this.charInitLabel.AutoSize = true;
+            this.charInitLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.charInitLabel.Location = new System.Drawing.Point(651, 51);
+            this.charInitLabel.Name = "charInitLabel";
+            this.charInitLabel.Size = new System.Drawing.Size(59, 16);
+            this.charInitLabel.TabIndex = 8;
+            this.charInitLabel.Text = "Initiative:";
+            // 
+            // charInitBox
+            // 
+            this.charInitBox.Location = new System.Drawing.Point(710, 50);
+            this.charInitBox.Name = "charInitBox";
+            this.charInitBox.Size = new System.Drawing.Size(55, 20);
+            this.charInitBox.TabIndex = 7;
+            this.charInitBox.ValueChanged += new System.EventHandler(this.charInitBox_ValueChanged);
+            // 
+            // createCharButton
+            // 
+            this.createCharButton.Location = new System.Drawing.Point(544, 21);
+            this.createCharButton.Name = "createCharButton";
+            this.createCharButton.Size = new System.Drawing.Size(100, 23);
+            this.createCharButton.TabIndex = 6;
+            this.createCharButton.Text = "Create Character";
+            this.createCharButton.UseVisualStyleBackColor = true;
+            this.createCharButton.Click += new System.EventHandler(this.createCharButton_Click);
+            // 
+            // charNameBox
+            // 
+            this.charNameBox.Location = new System.Drawing.Point(544, 50);
+            this.charNameBox.Name = "charNameBox";
+            this.charNameBox.Size = new System.Drawing.Size(100, 20);
+            this.charNameBox.TabIndex = 4;
+            this.charNameBox.Text = "Character Name";
+            // 
+            // charListBox
+            // 
+            this.charListBox.FormattingEnabled = true;
+            this.charListBox.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.charListBox.Location = new System.Drawing.Point(384, 21);
+            this.charListBox.Name = "charListBox";
+            this.charListBox.Size = new System.Drawing.Size(142, 537);
+            this.charListBox.TabIndex = 3;
+            this.charListBox.SelectedIndexChanged += new System.EventHandler(this.charListBox_SelectedIndexChanged);
+            // 
+            // addToTrackerButton
+            // 
+            this.addToTrackerButton.Location = new System.Drawing.Point(286, 21);
+            this.addToTrackerButton.Name = "addToTrackerButton";
+            this.addToTrackerButton.Size = new System.Drawing.Size(84, 23);
+            this.addToTrackerButton.TabIndex = 2;
+            this.addToTrackerButton.Text = "<<<";
+            this.addToTrackerButton.UseVisualStyleBackColor = true;
+            this.addToTrackerButton.Click += new System.EventHandler(this.addToTrackerButton_Click);
+            // 
+            // initTracker
+            // 
+            this.initTracker.GridLines = true;
+            this.initTracker.HideSelection = false;
+            this.initTracker.Location = new System.Drawing.Point(7, 21);
+            this.initTracker.Name = "initTracker";
+            this.initTracker.Size = new System.Drawing.Size(264, 537);
+            this.initTracker.TabIndex = 1;
+            this.initTracker.UseCompatibleStateImageBehavior = false;
+            this.initTracker.View = System.Windows.Forms.View.Details;
+            this.initTracker.SelectedIndexChanged += new System.EventHandler(this.initTracker_SelectedIndexChanged);
+            // 
+            // initiativeTrackerLabel
+            // 
+            this.initiativeTrackerLabel.AutoSize = true;
+            this.initiativeTrackerLabel.Location = new System.Drawing.Point(4, 4);
+            this.initiativeTrackerLabel.Name = "initiativeTrackerLabel";
+            this.initiativeTrackerLabel.Size = new System.Drawing.Size(86, 13);
+            this.initiativeTrackerLabel.TabIndex = 0;
+            this.initiativeTrackerLabel.Text = "Initiative Tracker";
+            // 
+            // DMTool
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 626);
+            this.Controls.Add(this.combatPanel);
+            this.Controls.Add(this.combatButton);
+            this.Name = "DMTool";
+            this.Text = "Dungeon Master Tool";
+            this.combatPanel.ResumeLayout(false);
+            this.combatPanel.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.charInitBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button combatButton;
+        private System.Windows.Forms.Panel combatPanel;
+        private System.Windows.Forms.ListView initTracker;
+        private System.Windows.Forms.Label initiativeTrackerLabel;
+        private System.Windows.Forms.ListBox charListBox;
+        private System.Windows.Forms.Button addToTrackerButton;
+        private System.Windows.Forms.TextBox charNameBox;
+        private System.Windows.Forms.Button createCharButton;
+        private System.Windows.Forms.NumericUpDown charInitBox;
+        private System.Windows.Forms.Label charInitLabel;
+        private System.Windows.Forms.Label charListLabel;
+        private System.Windows.Forms.Button editCharButton;
+        private System.Windows.Forms.Button delCharButton;
+        private System.Windows.Forms.Button initTrackerSortButton;
+    }
+}
+
---
+++ DMToolkit/DMTCore.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DMToolkit
+{
+    public partial class DMTool : Form
+    {
+        List<SimpleCharacter> charList = new List<SimpleCharacter>();
+        SimpleCharacter[] initTrackerArr = new SimpleCharacter[100];
+        List<SimpleCharacter> initTrackerList = new List<SimpleCharacter>();
+        int trackedCount = 0;
+
+        public DMTool()
+        {
+            InitializeComponent();
+            MakeInitTracker();
+        }
+
+        private void MakeInitTracker() {
+            initTracker.Columns.Add("Character Name", 200);
+            initTracker.Columns.Add("Initiative", 60);
+        }
+
+        private void initTracker_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void combatButton_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void charListBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (charListBox.SelectedItem != null)
+            {
+                editCharButton.Enabled = true;
+                delCharButton.Enabled = true;
+                SimpleCharacter chara = (SimpleCharacter)charListBox.SelectedItem;
+                charNameBox.Text = chara.Name;
+                charInitBox.Value = chara.Init;
+            }
+            else {
+                editCharButton.Enabled = false;
+                delCharButton.Enabled = false;
+            }
+        }
+
+        private void createCharButton_Click(object sender, EventArgs e)
+        {
+            SimpleCharacter chara = new SimpleCharacter(charNameBox.Text, (int)charInitBox.Value);
+            charListBox.DataSource = null;
+            charList.Add(chara);
+
+            charListBox.DataSource = charList;
+            charListBox.ValueMember = "Name";
+            charListBox.DisplayMember = "name";
+        }
+
+        private void charInitBox_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void addToTrackerButton_Click(object sender, EventArgs e)
+        {
+            if (charListBox.SelectedItem != null)
+            {
+                SimpleCharacter chara = (SimpleCharacter)charListBox.SelectedItem;
+                initTrackerArr[trackedCount] = chara;
+                initTrackerList.Add(chara);
+                trackedCount++;
+                String[] arr = { chara.Name, chara.Init.ToString() };
+                ListViewItem lvi = new ListViewItem(arr);
+                initTracker.Items.Add(lvi);
+            }
+        }
+
+        private ListViewItem[] makeLVIAFromArray(List<SimpleCharacter> initList) {
+            ListViewItem[] lvia = new ListViewItem[initList.Count];
+            int i = 0;
+            foreach (SimpleCharacter sc in initList) {
+                if (sc != null)
+                {
+                    String[] arr = { sc.Name, sc.Init.ToString() };
+                    lvia[i] = new ListViewItem(arr);
+                    i++;
+                }
+            }
+            return lvia;
+        }
+
+        private void sortTracker() {
+            initTracker.Sorting = SortOrder.Descending;
+        }
+
+        private void editCharButton_Click(object sender, EventArgs e)
+        {
+            if (charListBox.SelectedItem != null)
+            {
+                SimpleCharacter chara = new SimpleCharacter(charNameBox.Text, (int)charInitBox.Value);
+                charList[charListBox.SelectedIndex] = chara;
+                charListBox.DataSource = null;
+                //charList.RemoveAt(charListBox.SelectedIndex-1);
+                //charList.Insert(charListBox.SelectedIndex, chara);
+                charListBox.DataSource = charList;
+                charListBox.ValueMember = "Name";
+                charListBox.DisplayMember = "name";
+            }
+        }
+
+        private void initTrackerSortButton_Click(object sender, EventArgs e)
+        {
+            initTracker.Items.Clear();
+            initTrackerList = initTrackerList.OrderByDescending(SimpleCharacter => SimpleCharacter.Init).ToList();
+            ListViewItem[] lvia = makeLVIAFromArray(initTrackerList);
+            initTracker.Items.AddRange(lvia);
+        }
+    }
+    public class SimpleCharacter {
+        private string name;
+        private int init;
+        private int physDamage;
+        private int stunDamage;
+        private int physDamagePool;
+        private int stunDamagePool;
+
+        public SimpleCharacter(string charName, int initiative) {
+            this.name = charName;
+            this.init = initiative;
+        }
+
+        public string Name {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+        public int Init {
+            get { return this.init; }
+            set { this.init = value; }
+        }
+    }
+}
